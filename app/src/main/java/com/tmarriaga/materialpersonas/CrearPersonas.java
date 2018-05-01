f03891044400744fef227dd9891abb9e6915351f
Tommy <tmarriaga@gmail.com>
2018-04-30 19:09:39 -0500

    Estructura Inicial

---
+++ app/src/main/java/com/tmarriaga/materialpersonas/CrearPersonas.java
@@ -0,0 +1,13 @@
+package com.tmarriaga.materialpersonas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrearPersonas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_personas);
+    }
+}
